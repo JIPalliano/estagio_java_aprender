6a25681bbb21120fca7e93ee7ae251e3d9229c0e
JIPalliano <ismael_palliano190@hotmail.com>
2024-08-20 10:11:04 -0300

    Upando o projeto

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello and welcome!");
+        //"Aceita números de até 3 unidades decimais, sem o (byte).
+        //byte x = 100;
+        //"Aceita números de até 5 unidades decimais.
+        //short x = 10000;
+            //"Aceita números de até 10 unidades decimais.
+            //int x = 1000000000;
+            //"Aceita números de até 10 unidades decimais.
+            //long x = 1000000000;
+        int x = 2_147_483_647;
+        System.out.println(x);
+    }
+}
