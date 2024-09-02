45578ccbc0acda771c556cb20d03344bf1545886
JIPalliano <ismael_palliano190@hotmail.com>
2024-09-02 10:34:56 -0300

    Estudando como um arraylist se comporta com OOP

--- src/learning_OOP/Main.java
+++ src/learning_OOP/Main.java
@@ -1,11 +1,26 @@
 package learning_OOP;
 
 
+import learning_OOP.MobilePhone.*;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+
+        Contact contact = Contact.createContact("Ismael","81823207");
+        Contact contact2 = Contact.createContact("Patricia","1234856");
+        MobilePhone mobile = new MobilePhone(contact.getPhoneNumber());
+        mobile.addNewContact(contact);
+        mobile.addNewContact(contact2);
+        mobile.addNewContact(Contact.createContact("Lucas","6543321"));
+        //testando validação
+        mobile.addNewContact(contact);
+        mobile.printContacts();
+        System.out.println(mobile.queryContact("Ismael"));
+
+
 
 
     }
--- src/learning_OOP/MobilePhone/Contact.java
+++ src/learning_OOP/MobilePhone/Contact.java
@@ -3,10 +3,18 @@ package learning_OOP.MobilePhone;
 public class Contact {
     private String name, phoneNumber;
 
+    public Contact(String name, String phoneNumber){
+        this.name=name;
+        this.phoneNumber=phoneNumber;
+    }
+
     public String getName(){
         return this.name;
     }
     public String getPhoneNumber(){
-        return phoneNumber;
+        return this.phoneNumber;
+    }
+    public static Contact createContact(String name, String phoneNumber){
+        return new Contact(name, phoneNumber);
     }
 }
