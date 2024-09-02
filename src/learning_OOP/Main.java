5814d531dc7a54f13b207ad2905030119ff6efd2
JIPalliano <ismael_palliano190@hotmail.com>
2024-09-02 11:12:06 -0300

    testando

--- src/learning_OOP/Main.java
+++ src/learning_OOP/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
         Contact contact = Contact.createContact("Ismael","81823207");
         Contact contact2 = Contact.createContact("Patricia","1234856");
-        MobilePhone mobile = new MobilePhone(contact.getPhoneNumber());
         mobile.addNewContact(contact);
         mobile.addNewContact(contact2);
         mobile.addNewContact(Contact.createContact("Lucas","6543321"));
