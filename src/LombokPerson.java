d2e7c594a42713bb0d633071996bf368ebe97473
JIPalliano <ismael_palliano190@hotmail.com>
2024-09-03 11:04:05 -0300

    Estudando sobre lombok e seu uso

---
+++ src/LombokPerson.java
@@ -0,0 +1,8 @@
+import lombok.*;
+
+@Data
+@Builder
+public class LombokPerson {
+    private int age;
+    private String name;
+}
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@ import java.util.ArrayList;
 
 import learning_OOP.SortedArray;
 import loja.*;
+//import lombok.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        LombokPerson lp = new LombokPerson(1, "nome");
+        lp.setName("ismael");
+        System.out.println(lp.getName());
+        System.out.println(lp.toString());
 
     }
 }
