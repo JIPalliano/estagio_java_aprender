2cf9654fbdf2190bff4c9d6cb99ecfebc797b951
JIPalliano <ismael_palliano190@hotmail.com>
2024-09-04 14:48:27 -0300

    Revert "Reforçando merge"
    This reverts commit d19123d9717527f217d6985a9b1cd8d1613659e6.

--- src/learning_OOP/Main.java
+++ src/learning_OOP/Main.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Learning Merge!");
-        System.out.println("Please enter the path of the file you would like to read: ");
+        int x = 1;
     }
 }
