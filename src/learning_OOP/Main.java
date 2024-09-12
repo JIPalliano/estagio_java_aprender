b8c6a7e3ab7c049eef5a4ef3335c8df7405bbbf1
JIPalliano <ismael_palliano190@hotmail.com>
2024-09-12 15:36:16 -0300

    Brincando com Thread no Java

--- src/learning_OOP/Main.java
+++ src/learning_OOP/Main.java
@@ -6,9 +6,23 @@ import learning_OOP.Playlist.*;
 
 import java.util.*;
 
-public class Main {
+public class Main extends Thread {
+    public void run(){
+        for(int i=0;i<10000000; i++) {
+            System.out.println("Thread Running"+i);
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Main Started");
+        for(int i=0; i<=10; i++){
+            System.out.println("Contando: "+i);
+        }
+        Thread.sleep(5000);
+        Main main = new Main();
+        main.start();
 
-    public static void main(String[] args) {
-
+        for(int i=0; i<=1000000; i++){
+            System.out.println("2 Contando: "+i);
+        }
     }
 }
