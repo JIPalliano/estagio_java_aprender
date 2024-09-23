a3afc9807a28223cff55891ba67d7102a385ff00
JIPalliano <ismael_palliano190@hotmail.com>
2024-09-23 15:36:09 -0300

    Criando métodos de comparação

--- src/learning_optional/Main.java
+++ src/learning_optional/Main.java
@@ -4,8 +4,11 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        Client client1 = new Client("Ismael Palliano", Optional.empty());
+        Client client1 = new Client("Ismael Palliano", Optional.of(new Address("Rua Curumim", "467", "Gravtaí", "Rio Grande do Sul")));
+        Client client2 = new Client("Ismael Palliano", Optional.of(new Address("Rua Curumim", "467", "Gravtaí", "Rio Grande do Sul")));
         System.out.println(client1.getName()+" - "+client1.getAddress());
+        client1.addressExists();
+        System.out.println(Client.comparateAddress(client1.getAddress(), client2.getAddress()));
 
 
     }
