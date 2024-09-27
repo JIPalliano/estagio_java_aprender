ae4a1794657f8117649c0041c9eb49347f486f75
JIPalliano <ismael_palliano190@hotmail.com>
2024-09-26 21:12:05 -0300

    Ajeitando as collections

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        Map<Integer, ArrayList<Number>> map = new HashMap<>();
 
         long tempoInicial = System.currentTimeMillis();
 
-        DiferentsLists.creatHashMapList();
-        DiferentsLists.sendAllHashMapList();
+
+
 
         long tempoFinal = System.currentTimeMillis() - tempoInicial;
         System.out.println("O método foi executado em " + tempoFinal+" ms");
