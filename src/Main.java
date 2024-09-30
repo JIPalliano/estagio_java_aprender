63eb968696bd19af240561300e79b0b1e6bfc6ce
JIPalliano <ismael_palliano190@hotmail.com>
2024-09-30 13:58:21 -0300

    salvando

--- src/Main.java
+++ src/Main.java
@@ -15,25 +15,9 @@ public class Main {
 
         long tempoInicial = System.currentTimeMillis();
 
-//        list1.add(500000, 1);
-        list.add(5000000, 2);
-//        DiferentsLists.creatArrayList(list).add(500000, 1);
-//        DiferentsLists.creatArrayList(list).get(500000);
-//        DiferentsLists.creatArrayList(list).remove(500000);
-//        DiferentsLists.creatArrayList(list).removeAll(list);
-//        DiferentsLists.creatArrayList(list).set(500000, 20);
-//        DiferentsLists.sendAllArrayList(list);
-
-//        DiferentsLists.creatTreeSet().add(1);
-//        DiferentsLists.sendAllTreeSet();
-
-//        DiferentsLists.creatLinkedList().add(500000, 20);
-//        DiferentsLists.creatLinkedList().addFirst( 20);
-//        DiferentsLists.creatLinkedList().addLast(10);
-//        DiferentsLists.creatLinkedList().get(500000);
-//        DiferentsLists.creatLinkedList().remove(500000);
-//        DiferentsLists.creatLinkedList().set(500000, 20);
-//        DiferentsLists.sendAllLinkedList();
+//        list1.addLast(20);
+//        list.add(1000000, 1);
+//
 
         long tempoFinal = System.currentTimeMillis() - tempoInicial;
         System.out.println("O método foi executado em " + tempoFinal+" ms");
